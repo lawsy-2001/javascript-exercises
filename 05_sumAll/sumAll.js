877ba0b7cbf0baa26a8f251d995f095865994ce1
Andrew <166739925+lawsy-2001@users.noreply.github.com>
2024-05-16 13:45:29 +0100

    Completed 05 sumAll
    Updated JS file so that it passed all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
-};
-
+const sumAll = function(startNum, endNum) {
+    let sum = 0;
+    if (startNum < 0 || endNum < 0){
+        return 'ERROR'
+    }else if (typeof startNum !== "number" || typeof endNum !== "number"){
+        return 'ERROR'
+    }else if (startNum > endNum){
+        for(let i = endNum; i <= startNum; i++){
+            sum += i;
+        }
+    }else{
+        for(let i = startNum; i <= endNum; i++){
+            sum += i;
+        }
+    }
+    return sum;
+}
 // Do not edit below this line
 module.exports = sumAll;
