87987f154f995c45fa73c2ba768a7cbd337038f0
Andrew <166739925+lawsy-2001@users.noreply.github.com>
2024-05-16 13:10:45 +0100

    Completed 04 removeFromArray
    updated JS file so that the tests ran successfully

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+    let newArr = arr;
+    for(let i = 0; i < arguments.length; i++){
+        if (arr.includes(arguments[i])){
+            let index = newArr.indexOf(arguments[i])
+            newArr = newArr.toSpliced(index,1).filter((num) => num !== arguments[i] ); 
+        }
+    }
+    return newArr;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
